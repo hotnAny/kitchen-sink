05bbd7e0db12a9e6700fc248e9f69ea199bc606d
hotnAny <xiangchen@acm.org>
2024-04-04 21:15:32 -0700

    nsf automation

---
+++ latex/init_nsf.py
@@ -0,0 +1,27 @@
+import os, sys, shutil
+
+project_path = "./nsf_project"
+
+sub_dirs = ["paper_work", "others", "sandbox"]
+
+latex_res = ["xac.bib", "xac.sty", "split.py", "gitignore"]
+
+template_dir = "nsf_template"
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+       project_path = sys.argv[1]
+
+    # create the main dir and subdirs
+    os.makedirs(project_path)
+    for sub_dir in sub_dirs:
+        os.makedirs(project_path + "/" + sub_dir)
+
+    # copy the nsf proposal template
+    shutil.copytree(template_dir, project_path + "/proposal")
+
+    # copy some commonly used latex stuff
+    for res in latex_res:
+        shutil.copy(res, project_path + "/proposal")
+    # deal with gitignore
+    shutil.move(project_path + "/proposal/gitignore", project_path + "/proposal/.gitignore")
