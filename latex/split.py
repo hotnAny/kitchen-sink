36675a55eb4cca544e14233a203210ffbc732a94
Xiang 'Anthony' Chen <xiangchen@acm.org>
2024-11-14 11:16:49 -0800

    updated

--- latex/split.py
+++ latex/split.py
@@ -5,7 +5,7 @@ input_pdf_filename = "main.pdf"
 
 # TODO: edit this
 split_pages = [
-    # ["sub_file_name", start_page, end_page]
+    # ["sub_file_name", start_page, num_pages]
 ]
  
 # NOTE shouldn't have changed anything below
@@ -14,10 +14,10 @@ input_pdf = PdfReader(open(input_pdf_filename, "rb"), strict=False)
 cnt = 0
 for sp in split_pages:
     output = PdfWriter()
-    for i in range(sp[1]-1, sp[2]):
+    for i in range(sp[1]-1, sp[1] - 1 + sp[2]):
         output.add_page(input_pdf.pages[i])
     pdf_out = str(cnt) + '_' + sp[0] + ".pdf"
     with open(pdf_out, "wb") as output_stream:
         output.write(output_stream)
     print(pdf_out)
-    cnt+=1
+    cnt += 1
